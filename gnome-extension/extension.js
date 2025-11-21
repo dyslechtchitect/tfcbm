09cfc5ed4be1dc707afeb067c0b446f9624e6194
ron neuman <ron.neuman@gmail.com>
2025-11-21 17:38:19 +0200

    fdsfsdfd

--- gnome-extension/extension.js
+++ gnome-extension/extension.js
@@ -1,12 +1,15 @@
 import { Extension } from 'resource:///org/gnome/shell/extensions/extension.js';
 import * as Main from 'resource:///org/gnome/shell/ui/main.js';
 import Gio from 'gi://Gio';
+import GLib from 'gi://GLib';
 import { ClipboardMonitorService } from './src/ClipboardMonitorService.js';
 import { GnomeClipboardAdapter } from './src/adapters/GnomeClipboardAdapter.js';
 import { UnixSocketNotifier } from './src/adapters/UnixSocketNotifier.js';
 import { PollingScheduler } from './src/PollingScheduler.js';
 
-const APP_ID = 'org.tfcbm.ClipboardManager.desktop';
+const DBUS_NAME = 'org.tfcbm.ClipboardManager';
+const DBUS_PATH = '/org/tfcbm/ClipboardManager';
+const DBUS_IFACE = 'org.tfcbm.ClipboardManager';
 
 export default class ClipboardMonitorExtension extends Extension {
     constructor(metadata) {
@@ -14,12 +17,30 @@ export default class ClipboardMonitorExtension extends Extension {
     }
 
     _toggleUI() {
-        log('[TFCBM] Toggling UI...');
-        let app = Main.AppSystem.get_default().lookup_app(APP_ID);
-        if (app) {
-            app.activate();
-        } else {
-            log(`[TFCBM] App with ID ${APP_ID} not found.`);
+        log('[TFCBM] Toggling UI via DBus...');
+        try {
+            const timestamp = global.display.get_current_time_roundtrip();
+            Gio.DBus.session.call(
+                DBUS_NAME,
+                DBUS_PATH,
+                DBUS_IFACE,
+                'Activate',
+                new GLib.Variant('(u)', [timestamp]),
+                null,
+                Gio.DBusCallFlags.NONE,
+                -1,
+                null,
+                (connection, result) => {
+                    try {
+                        connection.call_finish(result);
+                        log('[TFCBM] UI activated successfully');
+                    } catch (e) {
+                        log('[TFCBM] Error activating UI: ' + e.message);
+                    }
+                }
+            );
+        } catch (e) {
+            log('[TFCBM] Error calling DBus: ' + e.message);
         }
     }
 
