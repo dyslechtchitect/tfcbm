df96e4cf184f8ff9fc63fbd3962370529b5a7a17
ron neuman <ron.neuman@gmail.com>
2025-11-21 17:11:18 +0200

    gawd

--- gnome-extension/extension.js
+++ gnome-extension/extension.js
@@ -1,5 +1,6 @@
 import { Extension } from 'resource:///org/gnome/shell/extensions/extension.js';
 import * as Main from 'resource:///org/gnome/shell/ui/main.js';
+import Gio from 'gi://Gio';
 import { ClipboardMonitorService } from './src/ClipboardMonitorService.js';
 import { GnomeClipboardAdapter } from './src/adapters/GnomeClipboardAdapter.js';
 import { UnixSocketNotifier } from './src/adapters/UnixSocketNotifier.js';
@@ -37,11 +38,26 @@ export default class ClipboardMonitorExtension extends Extension {
 
         try {
             log('[TFCBM] Attempting to add keybinding...');
-            const settings = this.getSettings();
-            log('[TFCBM] Got settings: ' + settings);
+            // Load settings from extension's schemas directory
+            log('[TFCBM] Extension dir: ' + this.dir.get_path());
+            const schemaDir = this.dir.get_child('schemas');
+            log('[TFCBM] Schema dir: ' + schemaDir.get_path());
+            const schemaSource = Gio.SettingsSchemaSource.new_from_directory(
+                schemaDir.get_path(),
+                Gio.SettingsSchemaSource.get_default(),
+                false
+            );
+            log('[TFCBM] Schema source created');
+            const schema = schemaSource.lookup('org.gnome.shell.extensions.simple-clipboard', false);
+            log('[TFCBM] Schema lookup result: ' + schema);
+            if (!schema) {
+                throw new Error('Schema not found');
+            }
+            this._settings = new Gio.Settings({ settings_schema: schema });
+            log('[TFCBM] Got settings: ' + this._settings);
             Main.wm.addKeybinding(
                 'toggle-tfcbm-ui',
-                settings,
+                this._settings,
                 0, // Gio.SettingsBindFlags.DEFAULT
                 1, // Shell.ActionMode.NORMAL
                 () => {
@@ -52,6 +68,7 @@ export default class ClipboardMonitorExtension extends Extension {
             log('[TFCBM] Keybinding added successfully');
         } catch (e) {
             log('[TFCBM] Error adding keybinding: ' + e);
+            log('[TFCBM] Error stack: ' + e.stack);
         }
     }
 
